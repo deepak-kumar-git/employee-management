11460c9923005a14bc24074a69324fe067a5d42b
Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>
2024-09-08 22:53:31 +0530

    dropdown component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
